8fd15d5699c8e5dcf994f7b920f79cee13223211
brandon <brandon.v.schiller@gmail.com>
2022-09-19 19:26:48 -0700

    beta release

--- playerdatabase.py
+++ playerdatabase.py
@@ -1,7 +1,5 @@
-import os
 import pandas as pd
 import sqlalchemy
-import sys
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.exc import IntegrityError
