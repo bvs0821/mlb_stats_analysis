1388859627577d6fa876cf16e18a3c75c7976cd4
brandon <brandon.v.schiller@gmail.com>
2022-09-20 13:30:20 -0700

    beta release

--- findplayerID.py
+++ findplayerID.py
@@ -39,13 +39,13 @@ for ID in all_player_ID:
             pass
 
 # outputs .txt files for playerIDs, pitcherIDs, and hitterIDs
-with open('pitcherID.txt', 'w') as file:
+with open('master_pitcherID.txt', 'w') as file:
     file.write('\n'.join(str(pitcher_ID) for pitcher_ID in pitcher_ID))
 
-with open('hitterID.txt', 'w') as file:
+with open('master_hitterID.txt', 'w') as file:
     file.write('\n'.join(str(hitter_ID) for hitter_ID in hitter_ID))
 
-with open('playerID.txt', 'w') as file:
+with open('master_playerID.txt', 'w') as file:
     file.write('\n'.join(str(all_player_ID) for all_player_ID in all_player_ID))
 
 print("All player IDs are logged.")
